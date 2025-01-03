224c2c3683583742be99490724a498845f452c2d
koraykck <koraykck73@gmail.com>
2025-01-03 16:26:21 +0300

    identity eklendi

---
+++ DataGenerator/DataAccess/Context/RDGContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class RDGContext : IdentityDbContext<AppUser, IdentityRole<int>, int>
+    {
+        public RDGContext(DbContextOptions<RDGContext> options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        DbSet<AppUser> AppUsers { get; set; }
+        DbSet<AppUserProfile> AppUserProfiles { get; set; }
+    }
+}
